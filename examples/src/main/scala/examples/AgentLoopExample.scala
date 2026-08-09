bf46a842461e112e5a055f995c41e1ac9dc6ceae
scala-steward <scala-steward>
2026-08-09 00:17:00 +0000

    Update claude, openai, ox to 0.6.0

--- examples/src/main/scala/examples/AgentLoopExample.scala
+++ examples/src/main/scala/examples/AgentLoopExample.scala
@@ -1,4 +1,4 @@
-//> using dep com.softwaremill.sttp.ai::openai:0.5.6
+//> using dep com.softwaremill.sttp.ai::openai:0.6.0
 //> using dep ch.qos.logback:logback-classic:1.5.38
 
 package examples
--- examples/src/main/scala/examples/ClaudeImageAnalysisExample.scala
+++ examples/src/main/scala/examples/ClaudeImageAnalysisExample.scala
@@ -1,4 +1,4 @@
-//> using dep com.softwaremill.sttp.ai::claude:0.5.6
+//> using dep com.softwaremill.sttp.ai::claude:0.6.0
 //> using dep ch.qos.logback:logback-classic:1.5.38
 
 // remember to set the ANTHROPIC_API_KEY env variable!
