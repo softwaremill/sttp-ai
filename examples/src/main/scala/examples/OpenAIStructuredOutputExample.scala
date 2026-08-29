103eaecaa7dbcfaac2918c8da4569532e454aefe
softwaremill-ci <ci@softwaremill.com>
2026-08-29 02:26:32 +0200

    Update claude, openai, ox to 0.10.0 (#609)

--- examples/src/main/scala/examples/OpenAIStructuredOutputExample.scala
+++ examples/src/main/scala/examples/OpenAIStructuredOutputExample.scala
@@ -1,4 +1,4 @@
-//> using dep com.softwaremill.sttp.ai::openai:0.8.0
+//> using dep com.softwaremill.sttp.ai::openai:0.10.0
 //> using dep ch.qos.logback:logback-classic:1.5.38
 
 // remember to set the OPENAI_API_KEY env variable!
