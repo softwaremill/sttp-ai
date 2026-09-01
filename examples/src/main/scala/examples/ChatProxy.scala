e3b158e9a1b87fdb331ad126b7ea70e1b86c290f
scala-steward <scala-steward>
2026-09-01 00:13:45 +0000

    Update claude, openai, ox to 0.11.0

--- examples/src/main/scala/examples/ChatProxy.scala
+++ examples/src/main/scala/examples/ChatProxy.scala
@@ -1,4 +1,4 @@
-//> using dep com.softwaremill.sttp.ai::ox:0.10.0
+//> using dep com.softwaremill.sttp.ai::ox:0.11.0
 //> using dep com.softwaremill.sttp.tapir::tapir-netty-server-sync:1.13.28
 //> using dep com.softwaremill.sttp.client4::ox:4.0.26
 //> using dep ch.qos.logback:logback-classic:1.5.38
