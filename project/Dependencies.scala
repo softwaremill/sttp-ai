8ee264f78e04627433a42173a6251404dba2acdd
scala-steward <scala-steward>
2026-09-26 00:11:20 +0000

    Update chimp-client, chimp-server to 0.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val tapir = "1.13.31"
     val circe = "0.14.16"
     val circeGenericExtras = "0.14.4" // 0.14.5 is only available as RC
-    val chimp = "0.5.2"
+    val chimp = "0.6.0"
   }
 
   object Libraries {
